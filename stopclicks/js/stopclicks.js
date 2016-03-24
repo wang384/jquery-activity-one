ae1af007dc9d585e781a5ebc8bb74fbcc9eb6000
Daniel Cloud <daniel@danielcloud.org>
2016-03-23 21:29:17 -0400

    Update instruction text

--- stopclicks/js/stopclicks.js
+++ stopclicks/js/stopclicks.js
@@ -2,7 +2,8 @@ $(document).ready(function () {
 
   /*
     The goal here is to stop default action when you click a link
-    (going to another page), and instead
+    (going to another page), and instead create a new element as
+    a string and append it to the article element(s).
   */
   $("a").click(function (event) {
     event.preventDefault();
