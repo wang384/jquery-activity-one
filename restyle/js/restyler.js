b88e29cf661f4f834e83e3c94da7dbfd59201c08
Daniel Cloud <daniel@danielcloud.org>
2016-03-23 20:47:11 -0400

    Restyle solved

---
+++ restyle/js/restyler.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  var switchOn = true;
+  var crimson = "#7D110C";
+  var cream = "#E1D8B7"
+
+  // http://api.jquery.com/click/
+  $("p").click(function () {
+    /*
+      We want to use a value to represent on/off.
+      "if" lets us check a condition (in the parentheses after the "if" keyword)
+      and execute the code inside the { } if the conditional expression is true
+      "else" provides a way for us to do something else when
+      the conditional expression is *not* true.
+      https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else
+    */
+    if (switchOn) {
+      // http://api.jquery.com/css/
+      $("body").css("background-color", crimson);
+      switchOn = false;
+    } else {
+      $("body").css("background-color", cream);
+      switchOn = true;
+    }
+  });
+});
